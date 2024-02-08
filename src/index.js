bc00d1274f40482384f996e31faf455d193c3d82
Allyssa <asalbores7@gmail.com>
2024-02-08 22:16:39 +0800

    Add token

--- src/index.js
+++ src/index.js
@@ -13,18 +13,19 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
+// Read the token from environment variable
+const token = process.env.REACT_APP_GITHUB_TOKEN;
+
 const httpLink = createHttpLink({
   uri: "https://api.github.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: "bearer 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",
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
